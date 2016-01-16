cfee16981b7379b97877c0d4e8d514abc779250c
Minkyu Kim <thechaos16@gmail.com>
2016-01-16 16:59:43 +0900

    lasso and elastic net

--- python/Regressor.py
+++ python/Regressor.py
@@ -9,26 +9,46 @@ import numpy as np
 import sklearn.linear_model as lm
 
 class Regressor:
-    def __init__(self,X,Y,opt=None):
+    def __init__(self,X,Y,opt={}):
         self.X = X
         self.Y = Y
         self.opt = opt
     
-    def linearRegression(self):
+    def linear_regression(self):
         clf = lm.LinearRegression()
         clf.fit(self.X,self.Y)
         return clf.coef_
         
-    def RidgeRegression(self):
+    def ridge_regression(self):
         clf = lm.Ridge(alpha=1.0)
         clf.fit(self.X,self.Y)
         return clf.coef_
         
+    def lasso(self):
+        try:
+            alpha = self.opt['alpha']
+        except KeyError:
+            alpha = 0.1
+        clf = lm.Lasso(alpha=alpha)
+        clf.fit(self.X,self.Y)
+        return clf.coef_
+        
+    def elastic_net(self):
+        try:
+            alpha = self.opt['alpha']
+        except KeyError:
+            alpha = 1.0
+        clf = lm.ElasticNet(alpha=alpha)
+        clf.fit(self.X,self.Y)
+        return clf.coef_
+        
 if __name__=='__main__':
-    inputMat = np.array([np.random.randn(100),np.random.randn(100),np.random.randn(100)]).T
-    outputMat = np.random.randn(100)
+    input_mat = np.array([np.random.randn(100),np.random.randn(100),np.random.randn(100)]).T
+    output_mat = np.random.randn(100)
     
-    reg = Regressor(inputMat,outputMat)
-    lincoeff = reg.linearRegression()
-    ridcoeff = reg.RidgeRegression()
+    reg = Regressor(input_mat,output_mat)
+    lin_coeff = reg.linear_regression()
+    rid_coeff = reg.ridge_regression()
+    lasso_coeff = reg.lasso()
+    ela_coeff = reg.elastic_net()
     
