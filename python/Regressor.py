e454356a9caee7d920dec2f37dcf3df5cc9b504e
minkyu <mkkim@solidware.io>
2016-01-18 16:42:11 +0900

    regression test

--- python/Regressor.py
+++ python/Regressor.py
@@ -34,19 +34,25 @@ class Regressor:
         return clf.coef_
         
     def elastic_net(self):
-        try:
+        if 'alpha' in self.opt:
             alpha = self.opt['alpha']
-        except KeyError:
+        else:
             alpha = 1.0
-        clf = lm.ElasticNet(alpha=alpha)
+        if 'l1_ratio' in self.opt:
+            l1_ratio = self.opt['l1_ratio']
+        else:
+            l1_ratio = 0.5
+        clf = lm.ElasticNet(alpha=alpha,l1_ratio=l1_ratio)
         clf.fit(self.X,self.Y)
         return clf.coef_
         
 if __name__=='__main__':
-    input_mat = np.array([np.random.randn(100),np.random.randn(100),np.random.randn(100)]).T
+    input_mat = np.array([np.sort(np.random.randn(100)),np.random.randn(100),np.random.randn(100)]).T
     output_mat = np.random.randn(100)
+    sorted_output_mat = np.sort(output_mat)
     
     reg = Regressor(input_mat,output_mat)
+    #reg = Regressor(input_mat,sorted_output_mat)    
     lin_coeff = reg.linear_regression()
     rid_coeff = reg.ridge_regression()
     lasso_coeff = reg.lasso()
