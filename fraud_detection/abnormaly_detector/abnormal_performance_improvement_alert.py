629337128cd1dc30d7eb316dc36a6426d39ba988
minkyu <mkkim@solidware.io>
2016-03-29 16:23:39 +0900

    basic architecture

---
+++ fraud_detection/abnormaly_detector/abnormal_performance_improvement_alert.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 16:13:46 2016
+
+@author: minkyu
+"""
+
+class PerformanceAlert():
+    def __init__(self, user_id, db_path, opt = {}):
+        self.user_id = user_id
+        self.db_path = db_path
+        self.opt = opt
+        
+    def __read_database(self):
+        pass
+    
+    def abnormal_improvement(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ fraud_detection/abnormaly_detector/fraud_detector.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 15:48:17 2016
+
+@author: minkyu
+"""
+
+class FraudDetector():
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
