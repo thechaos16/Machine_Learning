c5f94ab9cb28389cff533d74b39bebe31712f2b0
minkyu <mkkim@solidware.io>
2016-04-04 11:04:04 +0900

    just small update

--- fraud_detection/abnormaly_detector/abnormal_performance_improvement_alert.py
+++ fraud_detection/abnormaly_detector/abnormal_performance_improvement_alert.py
@@ -6,16 +6,24 @@ Created on Tue Mar 29 16:13:46 2016
 """
 
 class PerformanceAlert():
-    def __init__(self, user_id, db_path, opt = {}):
+    def __init__(self, user_id, db_path, domain, opt = {}):
         self.user_id = user_id
         self.db_path = db_path
+        self.domain = domain
         self.opt = opt
         
+    ## firstly, figure out how to build database
     def __read_database(self):
         pass
     
-    def abnormal_improvement(self):
+    ## performance is defined differently along domain
+    def abnormal_peformance_change(self):
         pass
+    
+    ## habit is defined differently along domain
+    def abnormal_habit_change(self):
+        pass
+    
 
 
 if __name__ == '__main__':
--- fraud_detection/abnormaly_detector/fraud_detector.py
+++ fraud_detection/abnormaly_detector/fraud_detector.py
@@ -5,9 +5,25 @@ Created on Tue Mar 29 15:48:17 2016
 @author: minkyu
 """
 
+import sys
+try:
+    import abnormaly_detector.abnormal_performance_improvement_alert as p_alert
+except ImportError:
+    sys.path.append('../')
+    import abnormaly_detector.abnormal_performance_improvement_alert as p_alert
+    
+
 class FraudDetector():
-    def __init__(self):
-        pass
+    def __init__(self, user_id, db_path, domain):
+        self.user_id = user_id
+        self.db_path = db_path
+        self.domain = domain
+        
+    ## using abnormal performance alert module
+    def abnormaly_detect(self):
+        performance_alert = p_alert.PerformanceAlert(self.user_id, self.db_path, self.domain)
+        
+
 
 if __name__ == '__main__':
     pass
