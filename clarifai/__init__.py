fc5469d51f671daf9c5044dd1ff3a9d0f81c0040
minkyu <mkkim@solidware.io>
2016-04-29 17:19:14 +0900

    clarifai update

---
+++ clarifai/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 17:10:16 2016
+
+@author: minkyu
+"""
+
---
+++ clarifai/test_tutorial.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 17:13:52 2016
+
+@author: minkyu
+"""
+
