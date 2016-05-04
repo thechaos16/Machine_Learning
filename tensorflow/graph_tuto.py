a6e9ee8b7af9b0aba93a6c120e558da2de3813ae
minkyu <mkkim@solidware.io>
2016-05-04 11:59:48 +0900

    some change

--- tensorflow/graph_tuto.py
+++ tensorflow/graph_tuto.py
@@ -1,7 +1,30 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow.python.platform
 import tensorflow as tf
+import input_data as ind
+import numpy as np
+import os
 
-matrix1 = tf.constant([[3.,3.]])
+class TensorflowWithGraph:
+    def __init__(self, file_path, batch_size=1000):
+        if not os.path.isdir(file_path):
+            raise FileNotFoundError(file_path + ' does not exist!')
+        self.data = self.__read_input(file_path)
+        self.batch_size = batch_size
+        # make empty structure
+        self.images = tf.placeholder(tf.float32, shape=(self.batch_size, len(self.data.train.images[0])))
+        self.labels = tf.placeholder(tf.float32, shape=(self.batch_size))
+    
+    def __read_input(self, file_path):
+        mnist = ind.read_data_sets(file_path, one_hot=True)
+        return mnist
+        
+    def graph_structure(self):
+        pass
 
-matrix2 = tf.constant([[2.],[2.]])
 
-product = tf.matmul(matrix1,matrix2)
+if __name__ == '__main__':
+    aaa = TensorflowWithGraph('./MNIST_data')
