eae8fa3370581253b4b4e657d5c4223d96bb1398
minkyu <mkkim@solidware.io>
2016-05-10 11:55:53 +0900

    unittest format updated

---
+++ fraud_detection/test/test_statistical_difference.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 16:41:03 2016
+
+@author: minkyu
+"""
+import unittest
+
+
+class TestStatisticalDifference(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
