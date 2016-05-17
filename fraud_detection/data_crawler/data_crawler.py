ada94d56903bf8543ccce0ad9d39bc79a15d9a0c
minkyu <mkkim@solidware.io>
2016-05-17 16:48:56 +0900

    statcast data crawler

--- fraud_detection/data_crawler/data_crawler.py
+++ fraud_detection/data_crawler/data_crawler.py
@@ -9,10 +9,19 @@ Created on Tue Mar 29 16:55:55 2016
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import chardet
+import json
 
 class URLHandler():
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, **kwargs):
+        if 'url' not in kwargs:
+            raise KeyError('URL should be in input argument!')
+        self.url = str(kwargs['url'])
+        if len(kwargs) >= 2:
+            self.url += '?'
+        for key, data in kwargs.items():
+            if key == 'url':
+                continue
+            self.url += (key + '=' + str(data) + '&')
         self.urldata = urlopen(url)
     
     # make url as string
@@ -23,7 +32,35 @@ class URLHandler():
         new_url_data = str(data.decode(encoding['encoding']))
         return new_url_data
     
-    def html_parser(self):
+    def html_parser(self, data_from='espn'):
+        if data_from.lower() == 'espn':
+            return self.data_from_espn()
+        elif data_from.lower() == 'statcast':
+            return self.data_from_baseball_savant()
+        elif data_from.lower() == 'battlenet':
+            return self.data_from_battle_net()
+        else:
+            raise NotImplementedError()
+    
+    # battle net
+    def data_from_battle_net(self):
+        pass
+    
+    # statcast
+    # it returns list of dictionary
+    def data_from_baseball_savant(self):
+        url_data = self.url_parser()
+        soup = BeautifulSoup(url_data, 'lxml')
+        valid_list = soup.find_all('script')
+        for elm in valid_list:
+            if 'leaderboard_data' in str(elm):
+                data = str(elm).split('leaderboard_data = ')[1].split('</script>')[0]
+                data = data.replace('\n','')
+                data = data.replace(';','')
+        return json.loads(data)
+            
+    # espn play-by-play data
+    def data_from_espn(self):
         url_data = self.url_parser()
         soup = BeautifulSoup(url_data, 'lxml')
         valid_list = soup.find_all('table')
@@ -71,6 +108,6 @@ class URLHandler():
         pass
        
 if __name__=='__main__':
-    mm = URLHandler('http://espn.go.com/mlb/playbyplay?gameId=350615102')
-    #mm = URLHandler('https://docs.python.org/3/library/html.parser.html')    
-    dd = mm.html_parser()
+    url = 'https://baseballsavant.mlb.com/statcast_leaderboard'
+    mm = URLHandler(url = url, year = 2016, abs = 30, player_type = 'pitcher')
+    dd = mm.html_parser('statcast')
