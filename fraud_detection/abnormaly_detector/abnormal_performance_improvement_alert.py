d18b93dd4b976b72a3bbeb3ea42f27bfd048dd29
thechaos16 <thechaos16@gmail.com>
2016-07-01 17:16:41 +0900

    time series converting updated

--- fraud_detection/abnormaly_detector/abnormal_performance_improvement_alert.py
+++ fraud_detection/abnormaly_detector/abnormal_performance_improvement_alert.py
@@ -16,12 +16,13 @@ except ImportError:
     import statistical_difference as sd
 
 class PerformanceAlert:
-    def __init__(self, user_id, db_path, domain=None, opt={}):
+    def __init__(self, user_id, db_path, domain=None, opt={'time': 'year'}):
         self.user_id = user_id
         self.db_path = db_path
-        self.db = self.__read_database()
-        self.domain = self.__get_domain_knowledge(domain)
         self.opt = opt
+        self.original_db = self.__read_database()
+        self.time_series = self.__convert_data_into_time_series()
+        self.domain = self.__get_domain_knowledge(domain)
 
     ## set specific domain knowledge
     def __get_domain_knowledge(self, domain):
@@ -30,25 +31,32 @@ class PerformanceAlert:
     ## firstly, figure out how to build database
     def __read_database(self):
         # initialize DB variable
-        db = pd.DataFrame([])
-        # extract data from db
-        # for test, make a fake data
-        np.random.seed(42)
-        random_series = pd.Series(np.random.random(size=10000))
-        random_series[:5000]+=3.0
-        # plt.figure()
-        # plt.plot(random_series)
-        db['player_id'] = random_series
+        db = pd.read_csv(self.db_path)
         return db
-    
+
+    # convert normal data into time-series data
+    def __convert_data_into_time_series(self):
+        fields = list(self.original_db.columns)
+        user_id_idx = fields.index(self.user_id)
+        time_field_idx = fields.index(self.opt['time'])
+        db_dict = {}
+        for line in self.original_db.values:
+            id_data = line[user_id_idx]
+            time = line[time_field_idx]
+            if id_data not in db_dict:
+                db_dict[id_data] = {}
+            db_dict[id_data][time] = {fields[idx]: line[idx] for idx in range(len(line)) if fields[idx] != self.user_id and fields[idx] != self.opt['time']}            
+        return db_dict
+        
+        
     ## performance is defined differently along domain
     def abnormal_performance_change(self, player_id):
         # first draft: one-dimensional performance
-        extract_from_db = self.db[player_id]
+        extract_from_db = self.time_series[player_id]
         aa = sd.difference_by_gradient(extract_from_db)
         print(aa)
 
 
 if __name__ == '__main__':
-    kk = PerformanceAlert('','')
-    kk.abnormal_performance_change('player_id')
+    kk = PerformanceAlert('player_id','../data/batting.csv')
+    # kk.abnormal_performance_change('player_id')
