7735d6a49f2686c085708218fd019967bdce6d5f
thechaos16 <thechaos16@gmail.com>
2017-04-26 00:35:30 +0900

    make package which will use tensorflow

--- python/MRF/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  2 17:10:16 2016
-
-@author: minkyu
-"""
-
--- python/statistical/statsmodels.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 20 17:26:48 2016
-
-@author: minkyu
-"""
-
-import numpy as np
-import statsmodels as sm
