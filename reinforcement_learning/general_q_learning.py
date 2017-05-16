ee8e5091c32242ed64d275528d66ef69904d99f5
thechaos16 <thechaos16@gmail.com>
2017-05-17 00:04:51 +0900

    error fixed and bellman equation added

--- reinforcement_learning/general_q_learning.py
+++ reinforcement_learning/general_q_learning.py
@@ -5,17 +5,28 @@ import numpy as np
 
 class QLearner:
     def __init__(self, initial_state, action_space, learning_rate=0.1, discount_rate=0.9, epsilon=0.1):
+        """
+        
+        :param initial_state: 
+        :param action_space: size of possible actions
+        :param learning_rate: learning rate for update q table (alpha in Bellman equation)
+        :param discount_rate: discount rate for future q value (gamma in Bellman equation)
+        :param epsilon: probability for exploration
+        """
         self.action_space = action_space
         self.learning_rate = learning_rate  # alpha
         self.discount_rate = discount_rate  # gamma
         self.epsilon = epsilon
-        self.q_table = dict({})
+        self.q_table = []
         self.state_hash_map = []
         self._initialize_q_table(initial_state)
 
     def _add_new_state(self, state):
         self.state_hash_map.append(state)
+        action_list = np.random.random(len(self.action_space))  # arbitrary action value
+        self.q_table.append(action_list)
 
+    # NOTE: since neural network is already implemented in cart_pole_agent, in here, using other method like grid
     def _state_approximation(self, state):
         for key in range(len(self.state_hash_map)):
             if self.state_hash_map[key] == state:
@@ -23,6 +34,7 @@ class QLearner:
         return -1
 
     def _initialize_q_table(self, initial_state):
+        # Note: late-update q-table after seeing states
         self._add_new_state(initial_state)
 
     def _update_q_table(self, state, action, reward, next_state):
@@ -32,16 +44,20 @@ class QLearner:
             # TODO: add new state, action tuple and update it with reward and future value)
             self._add_new_state(state)
         else:
-            # reference: Q(s,a) = Q(s,a) + alpha * (reward + gamma * future_value - Q(s,a))
-            pass
+            # Bellman equation: Q(s,a) = Q(s,a) + alpha * (reward + gamma * future_value - Q(s,a))
+            state_key = self._state_approximation(state)
+            next_state_key = self._state_approximation(next_state)
+            self.q_table[state_key][action] += self.learning_rate *\
+                                               (reward + self.discount_rate * np.max(self.q_table[next_state_key]) -
+                                                self.q_table[state_key][action])
 
-    def train(self, state, action, reward, next_state):
+    def train(self, state):
+        # epsilon-greedy
+        if np.random.random() <= self.epsilon:
+            pass
         pass
 
     def action(self, state):
-        # epsilon-greedy
-        if np.random.random() <= self.epsilon:
-            return self.env.action_space.sample()  # FIXME: this format only works with openAI gym
         q_table_for_state = self.q_table[state]
         # check rewards
         max_val = -np.inf
@@ -51,6 +67,7 @@ class QLearner:
                 max_key = key
                 max_val = q_table_for_state[key]
         return max_key
+
 
 if __name__ == 'main':
     pass
